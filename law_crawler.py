b243be7370cfb8fc2c4267498549014db2ddd489
swparkdr <swparkdr1999@gmail.com>
2025-06-13 11:19:35 +0900

    Delete law_crawler.py

--- law_crawler.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def load_law_changes(filepath='data/law_changes.json'):
-    """Load mock law change data from JSON file."""
-    with open(filepath, 'r', encoding='utf-8') as f:
-        return json.load(f)
