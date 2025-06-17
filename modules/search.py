ea08d0fe80ac181517d2509a6e8d3b046671ffeb
swparkdr <swparkdr1999@gmail.com>
2025-06-17 14:26:24 +0900

    Delete modules directory

--- modules/search.py
+++
@@ -1,8 +0,0 @@
-
-def search_laws(laws_data, keyword):
-    """Search for keyword in law clauses."""
-    results = []
-    for clause in laws_data.get("laws", []):
-        if keyword in clause.get("조문내용", "") or keyword in clause.get("조문명", ""):
-            results.append(clause)
-    return results
