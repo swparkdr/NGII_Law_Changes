d6d161750712f6f3129f57a4b295e1ea2a5fc89b
swparkdr <swparkdr1999@gmail.com>
2025-06-19 01:18:43 +0900

    Delete utils.py

--- utils.py
+++
@@ -1,6 +0,0 @@
-from difflib import ndiff
-
-def highlight_differences(old_text, new_text):
-    """Highlight differences between two texts."""
-    diff = ndiff(old_text.split(), new_text.split())
-    return " ".join(diff)
